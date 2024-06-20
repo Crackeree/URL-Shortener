9c40e6d31b1666053e7a518083bf4b99f804a613
Crackeree <kazi.kuet@gmail.com>
2024-06-21 04:15:09 +0600

    Add all the remaining files

---
+++ src/main/java/com/myworld/adamant/AdamantApplication.java
@@ -0,0 +1,13 @@
+package com.myworld.adamant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdamantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdamantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myworld/adamant/AdamantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myworld.adamant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdamantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
